011d3eecb3fb43826170c2e884a3d6651b93b217
HyukpyoHong <hphong@kaist.ac.kr>
2020-09-22 10:28:34 +0900

    Change the name of the variable from 'coord_struct' to 'coord_cell'

--- CRN_theta_construction.m
+++ CRN_theta_construction.m
@@ -1,4 +1,4 @@
-function theta = CRN_theta_construction(start_point, coord_struct, elementary_basis, F)
+function theta = CRN_theta_construction(start_point, coord_cell, elementary_basis, F)
 number_of_elementary_path = rank(elementary_basis);
 stopover = sym(start_point);
 theta = sym(1);
@@ -7,9 +7,10 @@ syms jj
 for l = 1:number_of_elementary_path
     tmp = stopover + (jj-1) * elementary_basis(l,:)';
     stopover_cell = num2cell(tmp);
-    theta = theta * symprod(F{l}(stopover_cell{:}), jj, 1, coord_struct{l});
+%    assumeAlso(jj <= coord_struct{l});
+    theta = theta * symprod(F{l}(stopover_cell{:}), jj, 1, coord_cell{l});
     if l > 1
-        stopover = stopover + coord_struct{l-1} * elementary_basis(l-1,:)';
+        stopover = stopover + coord_cell{l-1} * elementary_basis(l-1,:)';
     end
 end
 end
